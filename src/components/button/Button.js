0774281c2f223cafd240f14750c1a34d5060a6e5
github username <31523060+elishevaelbaz@users.noreply.github.com>
2020-12-14 15:31:21 -0500

    add loading icon to submit button

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSync } from '@fortawesome/free-solid-svg-icons'
 
 import './Styles.scss';
 
@@ -7,6 +9,7 @@ const Button = (props) => {
   return (
     <div className={"submitButton " + (props.inactive ? "submitButton--inactive" : "")} onClick={props.handleSubmit} >
       {props.label}
+      <FontAwesomeIcon className={"submitButton__loader " + (props.isLoading ? "submitButton__loader__loading" : "submitButton__loader__hidden")} icon={faSync} />
     </div>
   )
 }
