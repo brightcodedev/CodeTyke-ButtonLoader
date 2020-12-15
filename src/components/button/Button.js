ffd5c50bd4db7e677b35e6d0a5b0c6c02f90db93
Jonah Ullman <jonahmullman@gmail.com>
2020-12-15 11:22:41 -0600

    Adds icon to submit button when loading is in progress

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -7,8 +7,10 @@ const Button = (props) => {
   return (
     <div className={"submitButton " + (props.inactive ? "submitButton--inactive" : "")} onClick={props.handleSubmit} >
       {props.label}
+      {props.isLoading && <i className={props.iconClass + "submitButton__icon"}></i>}
     </div>
   )
 }
 
 export default Button;
+
